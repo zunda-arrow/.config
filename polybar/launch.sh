2f78a0f95a0d33e44b71020bbeecbc979089de01
Lunarmagpie <Lunarmagpie@github.com>
2021-09-18 01:36:14 -0400

    updated polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,33 +5,12 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
-# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-
-# polybar bar1 &
-# polybar bar2 &
-
-# polybar monitori3 &
-# polybar monitor1 &
-# polybar monitor2 &
-
-# polybar alli3 &
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload backdrop &
-    MONITOR=$m polybar --reload barworkspaces &
-    MONITOR=$m polybar --reload dateandtime &
-    MONITOR=$m polybar --reload pulseaudio &
-
-  done
-else
-  polybar --reload backdrop &
-  polybar --reload barworkspaces &
-  polybar --reload dateandtime &
-  polybar --reload pulseaudio &
-fi
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    # MONITOR=$m polybar backdrop &
+    MONITOR=$m polybar barworkspaces &
+    MONITOR=$m polybar dateandtime &
+    MONITOR=$m polybar pulseaudio &
+done
 
 
 echo "Bars launched..."
