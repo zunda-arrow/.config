dea9f25ae0c3f11c9a22adbdb2724d41034cf91b
Lunarmagpie <bambolambo0@gmail.com>
2024-08-21 15:20:10 -0400

    Update desktop thingies

--- desktop-thingies/config2.py
+++ desktop-thingies/config2.py
@@ -3,13 +3,6 @@ from desktop_thingies import Texture
 framerate = 60
 monitor = "eDP-1"
 
-# objects = [
-    # Texture(
-        # texture="/home/lunar/Documents/Projects/WidgetTool/examples/reimu_fumo.png",
-        # scale=1 / 4,
-    # ),
-# ]
-
 files = [
     ("alice.png", 1),
     ("marisa.png", 1.1),
@@ -21,16 +14,6 @@ files = [
     ("ran.png", 1.1),
     ("remilia.png", 1),
     ("yukari.png", 1),
-    # ("alice.png", 1),
-    # ("marisa.png", 1.1),
-    # ("reimu.png", 1),
-    # ("sakuya.png", 1),
-    # ("chen.png", 1),
-    # ("flandre.png", 1),
-    # ("patchouli.png", 1),
-    # ("ran.png", 1.1),
-    # ("remilia.png", 1),
-    # ("yukari.png", 1),
 ]
 
 objects = []
@@ -41,6 +24,6 @@ for file, scale in files:
             mass=1,
             friction=0.1,
             texture=f"/home/jake/.config/desktop-thingies/{file}",
-            scale=.85 * 1 * scale,
+            scale=.85 * scale,
         )
     ]
